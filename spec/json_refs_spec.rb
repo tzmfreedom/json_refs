951b7ecf200c5c0214f26ac0df547c2b48a1b9a8
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2017-11-19 00:51:59 +0900

    Add test code

--- spec/json_refs_spec.rb
+++ spec/json_refs_spec.rb
@@ -5,73 +5,90 @@ RSpec.describe JsonRefs do
     expect(JsonRefs::VERSION).not_to be nil
   end
 
-  def desymbolize(hash)
-    hash.inject({}) do |h, (k, v)|
-      h[k.to_s] = v
-      h
-    end
-  end
-
-  it "dereference JSON reference" do
-    result = JsonRefs.(desymbolize({
-      integer: 123,
-      string: 'abc',
-      array: [1, '2', true],
-      hash: {
-        string: 'aaa',
-        integer: 111,
-        boolean: true,
+  let(:input) {
+    {
+      'integer' => 123,
+      'string' => 'abc',
+      'array' => [1, '2', true],
+      'hash' => {
+        'string' => 'aaa',
+        'integer' => 111,
+        'boolean' => true,
       },
-      refs_integer: {
+      'refs_integer' => {
         '$ref' => '#/integer'
       },
-      refs_string: {
+      'refs_string' => {
         '$ref' => '#/string'
       },
-      refs_array: {
+      'refs_array' => {
         '$ref' => '#/array'
       },
-      refs_hash: {
+      'refs_hash' => {
         '$ref' => '#/hash'
       },
-      nest_hash: {
-        a: {
-          b: {
-            c: {
+      'nest_hash' => {
+        'a' => {
+          'b' => {
+            'c' => {
               '$ref' => '#/hash'
             }
           }
         }
+      },
+      'local_path' => {
+        '$ref' => File.expand_path('../fixtures/referenced.json', __FILE__)
+      },
+      'remote_path' => {
+        '$ref' => 'http://httpbin.org/user-agent'
       }
-    }))
-    expect(result).to eq(desymbolize({
-      integer: 123,
-      string: 'abc',
-      array: [1, '2', true],
-      hash: {
-        string: 'aaa',
-        integer: 111,
-        boolean: true,
+    }
+  }
+
+  let(:expected) {
+    {
+      'integer' => 123,
+      'string' => 'abc',
+      'array' => [1, '2', true],
+      'hash' => {
+        'string' => 'aaa',
+        'integer' => 111,
+        'boolean' => true,
       },
-      refs_integer: 123,
-      refs_string: 'abc',
-      refs_array: [1, '2', true],
-      refs_hash: {
-        string: 'aaa',
-        integer: 111,
-        boolean: true,
+      'refs_integer' => 123,
+      'refs_string' => 'abc',
+      'refs_array' => [1, '2', true],
+      'refs_hash' => {
+        'string' => 'aaa',
+        'integer' => 111,
+        'boolean' => true,
       },
-      nest_hash: {
-        a: {
-          b: {
-            c: {
-              string: 'aaa',
-              integer: 111,
-              boolean: true,
+      'nest_hash' => {
+        'a' => {
+          'b' => {
+            'c' => {
+              'string' => 'aaa',
+              'integer' => 111,
+              'boolean' => true,
             }
           }
         }
+      },
+      'local_path' => {
+        'referenced' => {
+          'string' => 'aaa',
+          'integer' => 123,
+          'boolean' => true
+        }
+      },
+      'remote_path' => {
+        'user-agent' => 'Ruby'
       }
-    }))
+    }
+  }
+
+  it "dereference JSON reference" do
+    result = JsonRefs.(input)
+    expect(result).to eq(expected)
   end
 end
